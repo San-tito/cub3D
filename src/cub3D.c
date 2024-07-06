51bfa0b5095c0ea10333c14b04fdc6ce0a45fea4
San-tito <68120901+San-tito@users.noreply.github.com>
2024-07-06 16:04:00 +0200

    yeah

--- src/cub3D.c
+++ src/cub3D.c
@@ -12,17 +12,20 @@
 
 #include "cub3D.h"
 
-void	terminate(t_core core)
-{
-	mlx_terminate(core.mlx);
-}
-
 int	main(int argc, char **argv)
 {
 	t_core	core;
 
+	/* setup argc and argc to scene */
 	core.scene = create_scene(argc, argv);
+	/* setup MLX; create window; setup width, heitht, image */
+	// setup mlx ((void *)core.mlx, (void *)core.image);
+	/* setup callback */
+	// callback setup(core.mlx, core.scene);
+	/* rasterize in image */
+	// rendering(core.image, core.scene);
+	// separation of concerns  
 	rendering_setup(core);
-	terminate(core);
+	mlx_terminate(core.mlx);
 	return (EXIT_SUCCESS);
 }
