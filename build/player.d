10cdcb69c4ccad82f2f436c0b75c1cd4f41e17c4
Deivid Santiago Ordoñez Varela <deordone@car5s4.42barcelona.com>
2024-07-06 04:18:29 +0200

    feat: position and size of minimap v2

--- build/player.d
+++
@@ -1,1 +0,0 @@
-build/player.o: src/player.c
--- src/graphics.c
+++ src/graphics.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 22:01:04 by sguzman           #+#    #+#             */
-/*   Updated: 2024/07/06 04:08:58 by deordone         ###   ########.fr       */
+/*   Updated: 2024/07/06 04:18:00 by deordone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ static t_core *init_minimap(t_core *core)
 {
 	t_minimap min_map;
 
-	min_map.m.x = ft_div(div2(core->scene.width), 6);
-	min_map.m.y = ft_div(div2(core->scene.height), 4);
+	min_map.m.x = ft_div(core->scene.width, 12);
+	min_map.m.y = ft_div(core->scene.height, 7);
 	min_map.p.a = 0;
 	min_map.p.x = min_map.m.x;
 	min_map.p.y = min_map.m.y;
 	min_map.p.dx = cos(deg2rad(min_map.p.a)) * 5;
 	min_map.p.dy = sin(deg2rad(min_map.p.a)) * 5;
-	min_map.size = ft_div(div2(core->scene.height), 6);
+	min_map.size = div2(ft_div(core->scene.width, 8));
 	core->scene.minimap = min_map;
 	return (core);
 }
