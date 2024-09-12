b360d1a5e1c3426f16752a060fc96d67a87ccdcf
San-tito <68120901+San-tito@users.noreply.github.com>
2024-09-12 21:28:12 +0200

    bro, workea con el lodev

--- build/parse.d
+++
@@ -1,5 +0,0 @@
-build/parse.o: src/parse.c include/cub3D.h \
- libs/MLX42/include/MLX42/MLX42.h include/scene.h include/callback.h \
- include/dispose.h include/error.h libs/libftprintf/include/ft_printf.h \
- libs/libftprintf/include/../libft/libft.h include/parse.h \
- include/raycast.h include/cub3D.h include/render.h include/xmalloc.h
--- build/raycast.d
+++
@@ -1,6 +0,0 @@
-build/raycast.o: src/raycast.c include/cub3D.h \
- libs/MLX42/include/MLX42/MLX42.h include/scene.h include/callback.h \
- include/dispose.h include/error.h libs/libftprintf/include/ft_printf.h \
- libs/libftprintf/include/../libft/libft.h include/parse.h \
- include/raycast.h include/cub3D.h include/render.h include/xmalloc.h \
- include/print.h include/raycast.h
