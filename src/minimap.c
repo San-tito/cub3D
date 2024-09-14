c84b16f17948f513dcddd58150e9e859fbefd7f3
deo <>
2024-09-14 18:22:00 +0200

    feat: minimap add fill circle

--- src/minimap.c
+++ src/minimap.c
@@ -6,12 +6,12 @@
 /*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 18:46:40 by droied            #+#    #+#             */
-/*   Updated: 2024/09/13 20:36:24 by droied           ###   ########.fr       */
+/*   Updated: 2024/09/14 18:18:20 by droied           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minimap.h"
-
+/*
 static void	draw_circle_aux(mlx_image_t *img, int xc, int yc, int x, int y)
 {
 	put_pixel(img, xc + x, yc + y, 0xFF00FFFF);
@@ -45,16 +45,48 @@ static void	draw_circle(mlx_image_t *img, t_ivec c, int radius)
 		else
 			d = d + 4 * x + 6;
 	}
-}
+}*/
 
+void	draw_circle(mlx_image_t *img, t_scene scene, t_ivec c, int radius, int color) 
+{
+	(void)scene;
+	unsigned short int	i;
+	unsigned short int	j;
+	t_ivec m;
+
+	m = scene.player.pos;
+
+	i = 0;
+		j = 0;
+	while (i < radius * 2 && i + c.y < (int32_t)img->height) {
+		j = 0;
+		while (j < radius * 2 && j + c.x < (int32_t)img->width) {
+			if (((j - radius) * (j - radius)) +
+			((i - radius) * (i - radius)) <
+			(radius * radius))
+			{
+				 // if (scene.map.cells[m.y][m.x] == SPACE)	
+				 if (i == 100 && j == 100)
+					put_pixel(img, j + c.x, i + c.y, color);
+			/*	else if (scene.map.cells[i][j] == WALL)	
+					put_pixel(img, j + c.x, i + c.y, 0xFF00FFFF);*/
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("%i\n", i);
+	printf("%i\n", j);
+}
+/*
 static int	in_bounds(t_ivec c, t_ivec n, int r)
 {
 	int	square_distance;
 
 	square_distance = pow((n.x - c.x), 2) + pow((n.y - c.y), 2);
 	return (square_distance <= pow(r, 2));
-}
-
+}*/
+/*
 static int fill_circle(mlx_image_t *image, t_scene scene, t_ivec map, t_ivec pixel, uint32_t radius)
 {
 	if (!in_bounds(pixel, map, radius))
@@ -70,15 +102,27 @@ static int fill_circle(mlx_image_t *image, t_scene scene, t_ivec map, t_ivec pix
 	// if (--pixel.y && fill_circle(image, scene, map, pixel, radius) == 0)
 		// return (0);
 	return (1);
+}*/
+
+int	create_rgb(double r, double g, double b, int a)
+{
+	r = r / 255 * 0.5 * -0.1;
+	g = g / 255 * 0.5 * -0.1;
+	b = b / 255 * 0.5 * -0.1;
+	return ((int)r << 24 | (int)g << 16 | (int)b << 8 | a);
 }
 
-void	minimap(mlx_image_t *image, t_scene *scene)
+void	minimap(mlx_image_t *image, t_scene scene)
 {
 	t_minimap minimap;
 
-	minimap.pos.x = image->width / 8;
-	minimap.pos.y = image->height / 6;
+	// minimap.pos.x = image->width / 8;
+	// minimap.pos.y = image->height / 6;
+	minimap.pos.x = image->width / 136;
+	minimap.pos.y = image->height / 76;
 	minimap.radius = minimap.pos.x / 2;
-	draw_circle(image, minimap.pos, minimap.radius);
-	fill_circle(image, *scene, minimap.pos, minimap.pos, minimap.radius);
+	int color;
+	color = create_rgb(0, 0, 0, 50);
+	draw_circle(image, scene, minimap.pos, minimap.pos.x * 10, color);
+	//fill_circle(image, *scene, minimap.pos, minimap.pos, minimap.radius);
 }
--- src/raycast.c
+++ src/raycast.c
@@ -6,7 +6,7 @@
 /*   By: deordone <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 10:56:51 by deordone          #+#    #+#             */
-/*   Updated: 2024/09/13 18:27:00 by santito          ###   ########.fr       */
+/*   Updated: 2024/09/14 17:49:44 by droied           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,6 @@ static void	perform_dda(t_ray *ray, t_cell **cells)
 			ray->side = 1;
 		}
 		if (cells[(int)ray->pos.y][(int)ray->pos.x] > SPACE)
-			// we should handle the state of the door open
 			hit++;
 	}
 }
