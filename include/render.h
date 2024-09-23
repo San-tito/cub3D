cd6adb8fd0d1b2fe9745c9c2fd7f4bfb6883cc29
San-tito <68120901+San-tito@users.noreply.github.com>
2024-09-23 09:47:22 +0200

    this is working my dude

--- include/render.h
+++ include/render.h
@@ -6,7 +6,7 @@
 /*   By: droied <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:30:16 by droied            #+#    #+#             */
-/*   Updated: 2024/09/22 20:51:35 by santito          ###   ########.fr       */
+/*   Updated: 2024/09/23 09:32:52 by santito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define RENDER_H
 
 void	put_pixel(mlx_image_t *, unsigned int, unsigned int, unsigned int);
-void	game_loop(void *);
+int		get_pixel(mlx_texture_t *texture, unsigned int x, unsigned int y);
 void	draw_wall(mlx_image_t *, unsigned int, t_wall);
 void	draw_ceiling(mlx_image_t *, int, uint32_t, int);
 void	draw_floor(mlx_image_t *, int, uint32_t, int);
